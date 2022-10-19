5e395faa8ccc7af4ddc43953e59e7b888f2f6688
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-19 19:39:55 +0300

    Task 13 solution

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,4 +2,13 @@
 //2. на выходе показывает вторую цифру этого числа.
 // Например: 456 -> 5, 782 -> 8, 918 -> 1
 
-Console.WriteLine("Hello, World!");
+int SecondDigit (int Number)
+{
+    int SecDig = Number % 100 / 10;
+    return SecDig;
+}
+
+Console.WriteLine("Введите целое трехзначное число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int resalt = SecondDigit (Num);
+Console.WriteLine(resalt);
