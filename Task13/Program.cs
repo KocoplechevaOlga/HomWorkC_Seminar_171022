a83be80c063a165027772cdeaa5f1b70b64693ba
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-20 12:44:10 +0300

    Task 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Например: 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
+
+Console.WriteLine("Введите любое целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number % 10;
+
+
+if (number < 100) 
+{
+Console.WriteLine("В заданном числе отсутсвует третья цифра");
+}
+else
+{
+    while (count >= 10)
+    {
+        number = number / 10;
+        count = number % 10;
+    }
+//int resalt = number % 10;
+Console.WriteLine("Третья цифра заданного числа: ");
+Console.Write(count);
+}
+
