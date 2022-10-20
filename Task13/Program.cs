099057aa09f958309a96d187b77a84eaca968e2b
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-20 13:43:00 +0300

    Task 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,24 +1,24 @@
 ﻿// 1. выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // Например: 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
 
+int ThirdDig (int N)
+{
+    if (N < 100) Console.WriteLine("В заданном числе отсутсвует третья цифра");
+
+    else
+    {
+        while (N >= 1000)
+            {
+                N = N / 10;
+            }
+        Console.WriteLine($"Третья цифра заданного числа: {N % 10}");
+    }
+
+    return N;
+}
+
 Console.WriteLine("Введите любое целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int count = number % 10;
 
 
-if (number < 100) 
-{
-Console.WriteLine("В заданном числе отсутсвует третья цифра");
-}
-else
-{
-    while (count >= 10)
-    {
-        number = number / 10;
-        count = number % 10;
-    }
-//int resalt = number % 10;
-Console.WriteLine("Третья цифра заданного числа: ");
-Console.Write(count);
-}
-
+int hirdDigit = ThirdDig(number);
