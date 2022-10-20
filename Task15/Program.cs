cd6cbcb30b508b76ad51c1c00d06ff2501da675a
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-20 15:27:40 +0300

    Task 15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -2,4 +2,27 @@
 // и проверяет, является ли этот день выходным.
 // Например:   6 -> да, 7 -> да,1 -> нет
 
-Console.WriteLine("Hello, World!");
+bool FindWeekEnd (int N)
+{
+    if (N ==6 || N == 7) return true;
+    return false;
+}
+
+Console.WriteLine("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 1 && n < 7) 
+ {
+    bool result = FindWeekEnd (n);
+    if (result)
+    {
+    Console.WriteLine($"{n}  ->  Да");
+    }
+    else
+    {
+    Console.WriteLine($"{n}  ->  Нет");
+    }
+ }
+else
+{
+    Console.WriteLine("Введенное число не соответствует дню недели, ввидете число от 1 до 7");
+}
